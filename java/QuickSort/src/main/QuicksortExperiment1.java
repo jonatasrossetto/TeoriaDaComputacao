c12354af36d2516f4899f830a12e0a7bb19d1e2e
Jonatas Rossetto <jonatas.rossetto@gmail.com>
2024-01-06 08:22:00 -0300

    QuickSort: alterado o tipo para List e corrigido problema de indiceDireita<0

--- java/QuickSort/src/main/QuicksortExperiment1.java
+++ java/QuickSort/src/main/QuicksortExperiment1.java
@@ -1,90 +1,86 @@
 package main;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
 public class QuicksortExperiment1 {
 
 	public static void main(String[] args) {
 		System.out.println("QuicksortExperiment1 is running");
-//		long[] listaNumeros = new long[10];
-		long[] listaNumeros = new long[] { 4, 2, 8, 7, 1, 5, 3, 6 };
+
+		List<Long> listaNumeros = Arrays.asList(4L, 2L, 8L, 7L, 1L, 5L, 3L, 6L);
+
 		// gera lista de número randômicos
 //		for (int i = 0; i < 10; i++) {
 //			listaNumeros[i] = Math.round(Math.random() * 100);
 ////			System.out.println("lista["+i+"]: "+listaNumeros[i]);
 //		}
-		// imprime lista original de números para ordenação
-		for (int ii = 0; ii < listaNumeros.length; ii++) {
-			System.out.print(" [" + listaNumeros[ii] + "] ");
-		}
-		System.out.println("");
 
-		int indiceEsquerda = 0;
-		int indiceDireita = listaNumeros.length - 1;
-		long pivot = listaNumeros[0];
-		System.out.println("pivot= " + pivot);
+		imprimeVetor(listaNumeros);
+		QuickSort(listaNumeros);
 
-		while (indiceEsquerda <= indiceDireita) {
-
-			while (listaNumeros[indiceEsquerda] < pivot) {
-				indiceEsquerda++;
-//				System.out.println("indiceEsquerda= "+indiceEsquerda);
-			}
-
-			while (listaNumeros[indiceDireita] >= pivot) {
-				indiceDireita--;
-//				System.out.println("indiceDireita= "+indiceDireita);
-			}
-
-			if (indiceEsquerda <= indiceDireita) {
-				System.out.println("Swap, Esquerda: " + indiceEsquerda + " Direita: " + indiceDireita);
-				var tempEsquerda = listaNumeros[indiceEsquerda];
-				listaNumeros[indiceEsquerda] = listaNumeros[indiceDireita];
-				listaNumeros[indiceDireita] = tempEsquerda;
-			}
-
-			// imprime lista original de números para ordenação
-			for (int ii = 0; ii < listaNumeros.length; ii++) {
-				System.out.print(" [" + listaNumeros[ii] + "] ");
-			}
-			System.out.println("");
-
-		}
 	}
 
-	public void Quicksort(long[] vetor) {
+	public static List<Long> QuickSort(List<Long> vetor) {
+		
 		int indiceEsquerda = 0;
-		int indiceDireita = vetor.length - 1;
-		long pivot = vetor[0];
+		int indiceDireita = vetor.size() - 1;
+		long pivot = vetor.get(indiceEsquerda);
 		System.out.println("pivot= " + pivot);
 
 		while (indiceEsquerda <= indiceDireita) {
 
-			while (vetor[indiceEsquerda] < pivot) {
-				indiceEsquerda++;
-//				System.out.println("indiceEsquerda= "+indiceEsquerda);
+			if (vetor.size() == 2) {
+				if (vetor.get(0) > vetor.get(1)) {
+					return swapElements(vetor, 0, 1);
+				}
 			}
 
-			while (vetor[indiceDireita] >= pivot) {
+			while (vetor.get(indiceEsquerda) < pivot) {
+				indiceEsquerda++;
+				System.out.println("indiceEsquerda= "+indiceEsquerda);
+			}
+
+			while (vetor.get(indiceDireita) >= pivot) {
 				indiceDireita--;
-//				System.out.println("indiceDireita= "+indiceDireita);
+				System.out.println("indiceDireita= "+indiceDireita);
+				if (indiceDireita<0) break;
 			}
 
 			if (indiceEsquerda <= indiceDireita) {
 				System.out.println("Swap, Esquerda: " + indiceEsquerda + " Direita: " + indiceDireita);
-				var tempEsquerda = vetor[indiceEsquerda];
-				vetor[indiceEsquerda] = vetor[indiceDireita];
-				vetor[indiceDireita] = tempEsquerda;
+				vetor = swapElements(vetor, indiceEsquerda, indiceDireita);
 			}
-
 			imprimeVetor(vetor);
 		}
+		System.out.println("Fim, Esquerda: " + indiceEsquerda + " Direita: " + indiceDireita);
+
+		
+		
+		//		var vetorEsquerda = Arrays.copyOfRange(vetor, 0, indiceEsquerda);
+//		var vetorDireita = Arrays.copyOfRange(vetor, indiceEsquerda, vetor.length-1);
+//		imprimeVetor(vetorEsquerda);
+//		imprimeVetor(vetorDireita);
+//		vetorEsquerda=QuickSort(vetorEsquerda);
+//		vetorDireita=QuickSort(vetorDireita);
+		return vetor;
 	}
-	
-	public void imprimeVetor(long[] vetor) {
-		// imprime lista original de números para ordenação
-		for (int ii = 0; ii < vetor.length; ii++) {
-			System.out.print(" [" + vetor[ii] + "] ");
+
+	public static void imprimeVetor(List<Long> vetor) {
+		// imprime os elementos de vetor
+		for (int ii = 0; ii < vetor.size(); ii++) {
+			System.out.print(" [" + vetor.get(ii) + "] ");
 		}
 		System.out.println("");
+	}
+
+	public static List<Long> swapElements(List<Long> vetor, int indiceA, int indiceB) {
+		var temp = vetor.get(indiceA);
+		vetor.set(indiceB, vetor.get(indiceA));
+		vetor.set(indiceA, vetor.get(indiceB));
+		return vetor;
+
 	}
 
 }
