47e3f243d44146a1f3d86ad16a854edc54d0d3e2
Jonatas Rossetto <jonatas.rossetto@gmail.com>
2024-01-06 15:35:22 -0300

    QuickSort: corrigido condição de saída da recursão, algorítimo funcionando

--- java/QuickSort/src/main/QuicksortExperiment1.java
+++ java/QuickSort/src/main/QuicksortExperiment1.java
@@ -18,67 +18,71 @@ public class QuicksortExperiment1 {
 //		}
 
 		System.out.println(listaNumeros);
-		QuickSort(listaNumeros);
+		QuickSort(listaNumeros, 0, listaNumeros.size() - 1);
+		System.out.println("*** FIM *** " + listaNumeros);
 
 	}
 
-	public static List<Long> QuickSort(List<Long> vetor) {
-		
-		int indiceEsquerda = 0;
-		int indiceDireita = vetor.size() - 1;
+	public static void QuickSort(List<Long> vetor, int inicio, int fim) {
+		System.out.println("*** QuickSort Started ***");
+		System.out.println("inicio: "+inicio+" fim: "+fim);
+		System.out.println(vetor);
+		if (inicio >= fim) {
+			System.out.println("stop");
+			return;
+		}
+//		System.out.println(vetor.subList(inicio, fim+1));
+		int indiceEsquerda = inicio;
+		int indiceDireita = fim;
 		long pivot = vetor.get(indiceEsquerda);
 		System.out.println("pivot= " + pivot);
 		System.out.println(vetor.size());
 
 		while (indiceEsquerda <= indiceDireita) {
-
-			if (vetor.size() == 2) {
-				if (vetor.get(0) > vetor.get(1)) {
-					return swapElements(vetor, 0, 1);
+			if (fim - inicio == 1) {
+				System.out.println("fim - inicio == 1");
+				if (vetor.get(inicio) > vetor.get(fim)) {
+					swapElements(vetor, inicio, fim);
+					return;
 				}
+				return;
 			}
 
 			while (vetor.get(indiceEsquerda) < pivot) {
 				indiceEsquerda++;
-				System.out.println("indiceEsquerda= "+indiceEsquerda);
+//				System.out.println("indiceEsquerda= "+indiceEsquerda);
 			}
 
 			while (vetor.get(indiceDireita) >= pivot) {
 				indiceDireita--;
-				System.out.println("indiceDireita= "+indiceDireita);
-				if (indiceDireita<0) break;
+//				System.out.println("indiceDireita= "+indiceDireita);
+				if (indiceDireita < 0)
+					break;
 			}
 
 			if (indiceEsquerda <= indiceDireita) {
 				System.out.println("Swap, Esquerda: " + indiceEsquerda + " Direita: " + indiceDireita);
 				vetor = swapElements(vetor, indiceEsquerda, indiceDireita);
 			}
-			System.out.println(vetor);
 		}
-		System.out.println("Fim, Esquerda: " + indiceEsquerda + " Direita: " + indiceDireita);
-		
-		System.out.println(vetor.subList(0, indiceEsquerda));
-		System.out.println(vetor.subList(indiceDireita+1, vetor.size()));
 
+		System.out.println("Esquerda: " + indiceEsquerda + " Direita: " + indiceDireita);
+		System.out.println("esquerda");
+		QuickSort(vetor, inicio, indiceEsquerda-1);
+		System.out.println("Esquerda: " + indiceEsquerda + " Direita: " + indiceDireita);
 		
-		
-		//		var vetorEsquerda = Arrays.copyOfRange(vetor, 0, indiceEsquerda);
-//		var vetorDireita = Arrays.copyOfRange(vetor, indiceEsquerda, vetor.length-1);
-//		imprimeVetor(vetorEsquerda);
-//		imprimeVetor(vetorDireita);
-//		vetorEsquerda=QuickSort(vetorEsquerda);
-//		vetorDireita=QuickSort(vetorDireita);
-		return vetor;
+		System.out.println("direita");
+		QuickSort(vetor, indiceDireita+1, fim);
+		System.out.println("Esquerda: " + indiceEsquerda + " Direita: " + indiceDireita);
+
+
 	}
-
-	
 
 	public static List<Long> swapElements(List<Long> vetor, int indiceA, int indiceB) {
 		var temp = vetor.get(indiceB);
 		vetor.set(indiceB, vetor.get(indiceA));
 		vetor.set(indiceA, temp);
 		return vetor;
-
 	}
 
 }
